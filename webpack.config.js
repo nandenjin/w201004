ffd9a40eda1acc004b3f80ce0c0b705e73c44694
Kazumi Inada <hello@nandenjin.com>
2020-11-11 10:20:41 +0900

    chore: Remove used containers for builds

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,9 @@ module.exports = {
             loader: '@nandenjin/emcc-loader',
             options: {
               cwd: __dirname,
-              cc: 'docker run -v $(pwd):/src emscripten/emsdk emcc',
-              cxx: 'docker run -v $(pwd):/src emscripten/emsdk em++',
-              ld: 'docker run -v $(pwd):/src emscripten/emsdk emcc',
+              cc: 'docker run --rm -v $(pwd):/src emscripten/emsdk emcc',
+              cxx: 'docker run --rm -v $(pwd):/src emscripten/emsdk em++',
+              ld: 'docker run --rm -v $(pwd):/src emscripten/emsdk emcc',
               buildDir: `${__dirname}/temp`,
               commonFlags: ['-O3'],
               cFlags: ['-std=c11'],
